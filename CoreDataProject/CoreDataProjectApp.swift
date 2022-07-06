ea58cc5709c903ccff63212c51d90f95fecd650e
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-07-06 13:38:41 -0400

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Gabriel Marquez on 2022-07-06.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
